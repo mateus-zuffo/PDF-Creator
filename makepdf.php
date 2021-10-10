622396382856258efb58ac1ba17c92b3d5b5f679
Mateus Zuffo <mateushmdz@gmail.com>
2021-10-10 16:05:36 -0300

    Update

--- makepdf.php
+++ makepdf.php
@@ -1,34 +1,60 @@
 <?php
     require_once __DIR__ . '/vendor/autoload.php';
 
-    // Variáveis
+    //Variables
     $name = $_POST['name'];
     $turma = $_POST['turma'];
     $professora = $_POST['professora'];
-    $message = $_POST['message'];
     $primeiroParagrafo = $_POST['primeiroParagrafo'];
     $segundoParagrafo = $_POST['segundoParagrafo'];
     $terceiroParagrafo = $_POST['terceiroParagrafo'];
-    
+    $avanco = $_POST['avanco'];
+    $mereceatencao = $_POST['mereceatencao'];
+    $parecerfinal = $_POST['parecerfinal'];
 
+    //Create new PDF instance
     $html = "<table>
-                <thead>
-                    <tr>
-                        <th>Nº</th>
-                        <th>Objetivos de Aprendizagem e Desenvolvimento</th>
-                        <th>Sim</th>
-                        <th>Não</th>
-                        <th>Em Processo</th>
-                        <th>Não Trabalhado</th>
-                    </tr>
-                </thead>
-                <tbody>";
-                $html = addLinha($html,"1","Teste","1");
+    <thead>
+        <tr>
+            <th>Nº</th>
+            <th>Objetivos de Aprendizagem e Desenvolvimento</th>
+            <th>Sim</th>
+            <th>Não</th>
+            <th>Em Processo</th>
+            <th>Não Trabalhado</th>
+        </tr>
+    </thead>
+    <tbody>";
+    $html = addLinha($html,"1","Teste","1");
 
-                $html = $html ."  
-                </tbody>
-        </table>";
+    $html = $html ."  
+    </tbody>
+    </table>";
 
+    $data = '';    
+    $data .= '<h1> Relatório Individual Infantil</h1>';
+    $data .= '<div class="row mb-2">';
+    $data .= '<div class="col-md-6"><b> Nome:     </b>' . $name . '<br/></div>';
+    $data .= '<div class="col-md-6"><b> Turma:     </b>' . $turma . '<br/></div>';
+    $data .= '</div>';
+    $data .= '<b> Professora: </b>' . $professora . '<br/>';
+    if(isset($primeiroParagrafo)){
+        $data .= '<p> &emsp;&emsp;' . $primeiroParagrafo . '<p>';
+    }    
+    if(isset($segundoParagrafo)){
+        $data .= '<p> &emsp;&emsp;' . $segundoParagrafo . '<p>';
+    }
+    if(isset($terceiroParagrafo)){        
+        $data .= '<p> &emsp;&emsp;' . $terceiroParagrafo . '<p>';
+    }
+    $data .= $html . '<br/>';
+    $data .= '<p> Avanços: <p><br/>';
+    $data .= '<p> Merece Atenção: <p><br/>';
+    $data .= '<p> Parecer Final: <p><br/>';
+
+    $mpdf = new \Mpdf\Mpdf();
+    $mpdf->WriteHTML($data);
+    $mpdf->Output($name.'.pdf', 'D');
 
 
     function addLinha($html, $numero, $objetivo, $resposta){
@@ -76,29 +102,4 @@
 
         return $html;
     }
-//Create new PDF instance
-    $mpdf = new \Mpdf\Mpdf();
-
-    $data = '';
-
-    $data .= '<h1> Relatório Individual Infantil</h1>';
-    $data .= '<div class="row mb-2">';
-    $data .= '<div class="col-md-6"><b> Nome:     </b>' . $name . '<br/></div>';
-    $data .= '<div class="col-md-6"><b> Turma:     </b>' . $turma . '<br/></div>';
-    $data .= '</div>';
-    $data .= '<b> Professora: </b>' . $professora . '<br/>';
-    $data .= '<p> &emsp;&emsp;' . $primeiroParagrafo . '<p>';
-    $data .= '<p> &emsp;&emsp;' . $segundoParagrafo . '<p>';
-    $data .= '<p> &emsp;&emsp;' . $terceiroParagrafo . '<p>';
-    $data .= $html . '<br/>';
-    $data .= '<p> Avanços: <p><br/>';
-    $data .= '<p> Merece Atenção: <p><br/>';
-    $data .= '<p> Parecer Final: <p><br/>';
-    //ifs adicionar msg
-
-    //Wrute PDF
-    $mpdf->WriteHTML($data);
-    //Output to browser
-    $mpdf->Output($name.'.pdf', 'D');
-
 ?>
