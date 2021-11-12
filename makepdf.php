96e7b53f1f48aed358f8ad2de8b0398346255321
Mateus Zuffo <mateushmdz@gmail.com>
2021-11-11 23:27:59 -0300

    fix: makepdf bug

--- makepdf.php
+++ makepdf.php
@@ -4,11 +4,9 @@
     require_once __DIR__ . '/vendor/autoload.php';
     $name = $_POST['aluno'];
     $body = createContent();
-    $footer = createFooter();
 
     $mpdf = new \Mpdf\Mpdf();
     $mpdf->WriteHTML($body);
-    //$mpdf->SetFooter($footer);
     $mpdf->Output($name.'.pdf', 'D');
     
 ?>
